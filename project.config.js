faf9daf0a953e23675c9f3d2b81f27af446e9708
wangchao <504615732@qq.com>
2017-07-03 09:42:13 +0800

    添加了端口

--- project.config.js
+++ project.config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	// baseUrl: 'http://beegree.cc/api/v1/',
-	baseUrl: 'http://192.168.6.4/api',
+	baseUrl: 'http://192.168.6.4:3000/api',
 	// devBaseUrl: 'http://beegree.cc/api/v1/',
-	devBaseUrl: 'http://192.168.6.4/api',
+	devBaseUrl: 'http://192.168.6.4:3000/api',
 	appID: 'wx8d4be3fac8ce7cfd',
 	appSecret: '5d9325013fcd2a8e2b69b2c80f38b963',
 	weChatAuthorizationUrl: 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx8d4be3fac8ce7cfd&redirect_uri=${}&response_type=code&scope=snsapi_userinfo#wechat_redirect',
