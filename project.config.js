ff432643a33c508eaa759040496ca1c064482e5b
wangchao <504615732@qq.com>
2017-07-04 10:14:32 +0800

    更换了secrect

--- project.config.js
+++ project.config.js
@@ -4,6 +4,6 @@ module.exports = {
 	// devBaseUrl: 'http://beegree.cc/api/v1/',
 	devBaseUrl: 'http://47.94.90.161/api',
 	appID: 'wx8d4be3fac8ce7cfd',
-	appSecret: '5d9325013fcd2a8e2b69b2c80f38b963',
+	appSecret: 'a7a7fa098cc90791685f9e3df7145ccd',
 	weChatAuthorizationUrl: 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx8d4be3fac8ce7cfd&redirect_uri=http%3A%2F%2Fwww.sxsnyj.com%2F&response_type=code&scope=snsapi_userinfo#wechat_redirect',
 }
