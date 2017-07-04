567eb739b3c3de0f8cde0027ebc3cb24b596394b
wangchao <504615732@qq.com>
2017-07-04 11:27:50 +0800

    微调

--- src/containers/Home/container/Home.js
+++ src/containers/Home/container/Home.js
@@ -69,21 +69,23 @@ export default class Home extends React.Component {
 		return (
 			<div style={style} className={styles.home}>
 				{this.props.weChatInfo &&
-					<div className={styles.tip}>
-						<h2>阿拉善首届啤酒节</h2>
-						<h2>请验证您信息, 一起狂欢!</h2>
-					</div>
-					<div className={styles.container}>
-						<div className={styles.flexItem}>
-							<label className={styles.customLabel + ' ' + styles.usernameLabel} htmlFor="username">姓&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;名:</label>
-							<input id="username" className={styles.customInput} onChange={this.onChange} placeholder="姓名" />
+					<div>
+						<div className={styles.tip}>
+							<h2>阿拉善首届啤酒节</h2>
+							<h2>请验证您信息, 一起狂欢!</h2>
 						</div>
-						<div className={styles.flexItem}>
-							<label className={styles.customLabel} htmlFor="idcard">身&nbsp;份&nbsp;证: </label>
-							<input id="idcard" className={styles.customInput} onChange={this.onChange} type="number" placeholder="身份证号码" />
-						</div>
-						<div className={styles.okButton}>
-							<button disabled={this.props.loading} onClick={this.handleOkButtonClick}>{buttonMessage}</button>
+						<div className={styles.container}>
+							<div className={styles.flexItem}>
+								<label className={styles.customLabel + ' ' + styles.usernameLabel} htmlFor="username">姓&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;名:</label>
+								<input id="username" className={styles.customInput} onChange={this.onChange} placeholder="姓名" />
+							</div>
+							<div className={styles.flexItem}>
+								<label className={styles.customLabel} htmlFor="idcard">身&nbsp;份&nbsp;证: </label>
+								<input id="idcard" className={styles.customInput} onChange={this.onChange} type="number" placeholder="身份证号码" />
+							</div>
+							<div className={styles.okButton}>
+								<button disabled={this.props.loading} onClick={this.handleOkButtonClick}>{buttonMessage}</button>
+							</div>
 						</div>
 					</div>
 				}
